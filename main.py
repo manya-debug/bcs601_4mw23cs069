bdfeeed627666c5340351a95d83f53568fd89896
Manya <manya@example.com>
2026-05-05 07:04:11 +0000

    Final working backend project

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', active_tab="hcf")
 
 @app.route('/hcf_lcm', methods=['POST'])
 def hcf_lcm():
@@ -19,24 +19,24 @@ def hcf_lcm():
     h = hcf(a, b)
     l = (a * b) // h
 
-    return render_template('index.html', hcf=h, lcm=l)
+    return render_template('index.html', hcf=h, lcm=l, active_tab="hcf")
 
 @app.route('/reverse', methods=['POST'])
 def reverse():
     text = request.form['text']
     reversed_text = text[::-1]
 
-    return render_template('index.html', reversed=reversed_text)
+    return render_template('index.html', reversed=reversed_text, active_tab="reverse")
 
 @app.route('/factorial', methods=['POST'])
 def factorial():
     n = int(request.form['num'])
 
     f = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         f *= i
 
-    return render_template('index.html', factorial=f)
+    return render_template('index.html', factorial=f, active_tab="factorial")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
